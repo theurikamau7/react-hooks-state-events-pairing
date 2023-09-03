08dbd494045231932686454089fcd234a79d41e1
IanKamau <theurikamau7@gmail.com>
2023-09-04 00:44:51 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import video from "../data/video.js";
+import Video from "./Video.js";
 
 function App() {
   console.log("Here's your data:", video);
 
+
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+    <div>
+      <Video video={video}/>
+      {video.u}
     </div>
   );
 }
